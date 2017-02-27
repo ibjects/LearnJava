d4f28a6d1c18c6ce5f19eac47d811dd53b4a928c
Chaudahry Talha <gameral@live.com>
2017-02-27 11:03:12 +0500

    First Commit

---
+++ LearningJava/src/HelloWorld.java
@@ -0,0 +1,46 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+		int myNumber; //declaring variable
+		
+		myNumber = 88; //Initializing variable
+		
+		//We can do this in one line too as
+		
+		int anotherNumber = 34;
+		
+		//printing a value in a variable
+		System.out.println(myNumber);
+		
+		//other types of int
+
+		short myShort = 847;
+		long myLong = 9445;
+		
+		//for floating number
+		double myDouble = 7.3243;
+		float myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBool = false;
+		
+		byte myByte = 122;
+		
+		//print all of them
+		System.out.println(anotherNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
+
+	}
+
+}
