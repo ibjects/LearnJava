d0552aec44cab2583704bceb9168fecb81660d8c
Chaudahry Talha <gameral@live.com>
2017-02-27 11:53:46 +0500

    Strings lecture completed

--- LearningJava/src/Texts.java
+++ LearningJava/src/Texts.java
@@ -3,6 +3,25 @@ public class Texts {
 
 	public static void main(String[] args) {
 		
+		//initialize and allocate three strings
+		String myText = "Hello";
+		String singleSpace = " ";
+		String name = "John";
+		
+		//initialize and allocate a new string which will concatenate
+		String greetPerson = myText + singleSpace + name;
+		
+		//printing the result
+		System.out.println(greetPerson);
+		
+		//Another way of printing the same result
+		System.out.println("Hello" + " " + name);
+		
+		//here is how to concatenate other types with String
+		int num = 7;
+		
+		System.out.println("Hello " + name + " your lucky number is: " + num);
+		
 		
 	}
 
