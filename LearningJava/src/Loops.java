f5561e658b7c53c136b2d75f649d40cf4a9212af
Chaudahry Talha <gameral@live.com>
2017-02-27 15:11:57 +0500

    Looping lecture without do while

---
+++ LearningJava/src/Loops.java
@@ -0,0 +1,44 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// We saw this way of declaring a boolean in HelloWorld.java
+		boolean myBool = true;
+		
+		//otherWays
+		boolean newBoolOne = 3 < 4; //this will set myBool true
+		boolean newBoolTwo = 5 < 1; // this will set myBool false
+		
+		System.out.println(myBool);
+		System.out.println(newBoolOne);
+		System.out.println(newBoolTwo);
+		
+		//while loop
+		int num = 10;
+		boolean newBoolThree = num < 20;
+		
+		while(newBoolThree)
+		{
+			//print the value of boolean
+			System.out.println("Values is " + newBoolThree);
+			//incrementing one in the condition to avoid infinite loop
+			num = num+1;
+			
+			//assigning the new condition to boolean
+			newBoolThree = num < 20;
+			
+		}
+		
+		//for loop
+		
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Hello " + i);
+			
+			//another way of printing
+			System.out.printf("Hello %d\n", i);
+		}
+		
+	}
+
+}
